b22aba404841cea159505e2bc0a0e81a1f4fa74a
Антон Федотов <anthony.fedotos@gmail.com>
2023-11-27 21:38:37 +0600

    успешный запуск /shop

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -12,6 +12,6 @@ class ShopController extends Controller
     public function index()
     {
         $products = Product::with('category')->get();
-        return view('shop.index', ['products' => $products]);
+        return $products;
     }
 }
